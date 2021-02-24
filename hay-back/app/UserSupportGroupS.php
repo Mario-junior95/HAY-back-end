1760518f9e511e360decc6fd849e16a738377a88
Mario-junior95 <karehmario@gmail.com>
2021-02-24 14:55:26 +0200

    .

--- hay-back/app/UserSupportGroupS.php
+++ hay-back/app/UserSupportGroupS.php
@@ -11,4 +11,15 @@ class UserSupportGroupS extends Model
     protected $fillable = [
         'user_id' , 'support_session_id' , 'time_ss_id'
     ];
+
+    public function user(){
+        return $this->belongsToMany(User::class, 'supportsessionss__user','id');
+    }
+
+    public function session(){
+        return $this->belongsToMany(Support_session_ss::class,'supportsessionss__user' , 'id', 'support_session_id');
+    }
+    public function time(){
+        return $this->belongsTo(Time_ss::class,'time_ss_id');
+    }
 }
